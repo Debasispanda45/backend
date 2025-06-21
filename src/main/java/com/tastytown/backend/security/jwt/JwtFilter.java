aa949a8e1342ae9887debe7eec94047abab1b9b0
Debasispanda45 <pandadebasis036@gmail.com>
2025-06-21 22:48:45 +0530

    filter implemented

---
+++ src/main/java/com/tastytown/backend/security/jwt/JwtFilter.java
@@ -0,0 +1,62 @@
+package com.tastytown.backend.security.jwt;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.tastytown.backend.entity.User;
+import com.tastytown.backend.repository.UserRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtUtils jwtUtils;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        try {
+            if (authHeader == null || !authHeader.startsWith("Bearer")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            String token = authHeader.substring(7);
+            String userId = jwtUtils.getUserId(token);
+
+            if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                User user = userRepository.findById(userId).orElseThrow();
+
+                var roles = List.of(new SimpleGrantedAuthority(user.getRole().toString()));
+
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, roles);
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            var problemDetail = org.springframework.http.ProblemDetail.forStatus(HttpStatus.UNAUTHORIZED);
+            problemDetail.setTitle("Invalid Token");
+            problemDetail.setDetail(e.getMessage());
+
+            response.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().println(problemDetail);
+        }
+
+    }
+}
