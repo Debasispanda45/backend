43a659516f4d661788b1beb2d8f7bb84aa52794b
Debasispanda45 <pandadebasis036@gmail.com>
2025-06-21 21:31:42 +0530

    security config implemented

--- src/main/java/com/tastytown/backend/repository/UserRepository.java
+++ src/main/java/com/tastytown/backend/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.tastytown.backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<> {
-    
+import com.tastytown.backend.entity.User;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUserEmail(String email);
 }
--- src/main/java/com/tastytown/backend/security/CustomUserDetailsService.java
+++ src/main/java/com/tastytown/backend/security/CustomUserDetailsService.java
@@ -2,17 +2,31 @@ package com.tastytown.backend.security;
 
 import java.util.List;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
+import com.tastytown.backend.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+  private final UserRepository repository;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-      return new User("", "", List.of());
-    }
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    var user = repository.findByUserEmail(email)
+        .orElseThrow(() -> new UsernameNotFoundException("User not exist with email." + email));
 
-   
+    return new User(
+        user.getUserEmail(),
+        user.getUserPassword(),
+        List.of(new SimpleGrantedAuthority(user.getRole().toString())));
+  }
+
 }
