c6f59b5e714b9a01b75f575fa360750b6e7bbc02
Debasispanda45 <pandadebasis036@gmail.com>
2025-06-28 23:58:16 +0530

    Implement order management features including order retrieval and status updates, enhance security configuration, and add application properties for production

--- src/main/java/com/tastytown/backend/config/SecurityConfig.java
+++ src/main/java/com/tastytown/backend/config/SecurityConfig.java
@@ -37,6 +37,8 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/**").permitAll()
 
                         .requestMatchers("/api/v1/cart/**").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/orders/**").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/orders/user").authenticated()
 
                         .requestMatchers(HttpMethod.POST, "/api/v1/foods/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/v1/foods/**").hasRole("ADMIN")
@@ -44,6 +46,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/v1/categories/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/v1/categories/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/v1/categories/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/v1/orders").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/orders/**").hasRole("ADMIN")
 
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
